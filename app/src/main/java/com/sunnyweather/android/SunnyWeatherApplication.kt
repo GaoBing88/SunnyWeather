169d4780b06e586cea09224c5216f6c2a0fa2eee
Tony <tony@gmail.com>
2026-04-30 05:51:47 +0800

    实现搜素全球城市数据功能。

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -4,15 +4,16 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
-class SunnyWeatherApplication:Application() {
-    companion object{
-        const val TOKEN="UeVYdUwDw7fmq4AS"
+class SunnyWeatherApplication : Application() {
+    companion object {
+        const val TOKEN = "UeVYdUwDw7fmq4AS"
+
         @SuppressLint("StaticFieldLeak")
-        lateinit var context:Context
+        lateinit var context: Context
     }
 
     override fun onCreate() {
         super.onCreate()
-        context=applicationContext
+        context = applicationContext
     }
 }
